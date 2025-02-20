5bc77b9ef6bd0b63c51c8f5a892aabb982b35c50
sosag <sosag@GAEL>
2025-02-19 21:11:12 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfaz_de_puntuacion_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double p = double.Parse(textBox1.Text);
+            if (p > 100)
+            {
+                textBox2.Text = "puntuacion alta";
+            }
+            else
+            {
+                textBox2.Text = "puntuacion baja hechale mas ganas";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
